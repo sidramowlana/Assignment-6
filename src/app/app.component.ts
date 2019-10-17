96927e11e07dffad93652d66294698a987ae0483
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2019-10-17 17:54:18 +0530

    recreating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment6Pipes';
+  filterStatus='';
+  appStatus = new Promise((resolve,reject)=>
+  {
+    setTimeout(() => {
+      resolve('stable')
+    }, 2000);
+  })
+  servers=[
+    {
+      instanceType: 'large',
+      name:'Server A',
+      status:'stable',
+      date: new Date(12, 1 ,2019)
+    },
+    {
+      instanceType: 'large',
+      name:'Server B',
+      status:'stable',
+      date: new Date(12, 1 ,2019)
+    },
+    {
+      instanceType: 'small',
+      name:'Server C',
+      status:'critical',
+      date: new Date(12, 1 ,2019)
+    },
+    {
+      instanceType: 'large',
+      name:'Server D',
+      status:'danger',
+      date: new Date(12, 1 ,2019)
+    },
+  ]
+
+  getStatusClass(server:{instanceType:string,anme:string,status:string,date:Date})
+  {
+    return{
+      'list-group-item-success':server.status==='stable',
+      'list-group-item-warning':server.status==='critical',
+      'list-group-item-danger':server.status==='danger'
+    };
+  }
+  onAddServer()
+  {
+    this.servers.push({
+      instanceType:'middle',
+      name:'Server F',
+      status:'critical',
+      date: new Date(1, 1, 2018)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }from '@angular/forms'
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
+import { Shorten } from './shorten.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe,
+    Shorten
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
